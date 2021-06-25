ff0455993d728a70635dfdfcc8d89ce6ec04e887
João Filipe Costa Coelho <74990822+jfilipecostacoelho@users.noreply.github.com>
2021-06-25 09:11:53 +0100

    Create pset1-mario_less.c

---
+++ Week1/pset1-mario_less.c
@@ -0,0 +1,48 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+void bricks(int n);
+
+int main(void)
+{
+    int n;
+
+    // Ask user for a width between 1 and 8
+    do
+    {
+        n = get_int("What is the bottom width? ");
+    }
+    while (n < 1 || n > 8);
+    
+    // Calls brick function
+    bricks(n);
+}
+
+void bricks(int n)
+{
+    //Calculates number of empty space
+    int k = n - 1;
+
+    // Starts building the tower
+    for (int i = 0; i < n; i++)
+    {
+
+        // Empty Spaces
+        for (int y = 0; y < k; y++)
+        {
+            printf(" ");
+        }
+
+        // Hashes
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+
+        // New line and reduce empty space on the next line
+        printf("\n");
+        k--;
+    }
+
+}
